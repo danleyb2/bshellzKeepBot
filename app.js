908e535c190102cf298011dd685fa8ecef1a0cb9
Nyaundi Brian <ndieksman@gmail.com>
2015-06-28 22:30:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,39 +14,32 @@ var config={
 };
 
 var server=http.createServer(function(req,res){
-    console.log(res);
     if (req.method=='GET' && req.url=='/') {
 	res.writeHead(200,"{'Content-type':'text/plain'}");
 	fs.createReadStream("./assets/log.txt").pipe(res);
 	}
    });
-
 var time=new sch.RecurrenceRule();
 time.dayOfWeek=[1,3,5];
 time.hour=6;
 time.minute=0;
-
 sch.scheduleJob(time,function(){
-    var myBot=new irc.Client(config.server,config.name,
+var myBot=new irc.Client(config.server,config.name,
     {
         channels:config.channels,
         userName:config.userName,
         realName:config.realName
     }
-    
     );
-      myBot.addListener("join",function(channel,who){
-        
+   myBot.addListener("join",function(channel,who){
      if (who==config.name) {
          logEvent(logfile,"Bot joinned "+channel);
          var msg="!keep danleyb2";
          myBot.say(channel,msg);
          logEvent(logfile,"Bot said:- "+msg);
        // myBot.part("#bshellz");
-         
      }
     });
-  
 });
 function insertDate(){
     var d = new Date(),
@@ -59,13 +52,11 @@ function insertDate(){
                     d.getSeconds()].join(':');
           return dformat;
 }
-
 function logEvent(fileto,str){
     fs.open(fileto,'a');
     fs.appendFile(fileto,insertDate()+" : "+str+"\n");
    
 }
-
 
 server.listen(3000,function () {
     logEvent(logfile,"Server running");
