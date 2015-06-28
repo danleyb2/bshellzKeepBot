33ca28e15e68b77a728aa1efb81dbadde9417319
Nyaundi Brian <ndieksman@gmail.com>
2015-06-28 17:05:50 +0300

    added function to log bot events

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http=require("http");
 var irc=require("irc");
 var sch=require("node-schedule");
+var fs=require("fs");
 
+var logfile="assets/log.txt";
 
 var config={
     channels:["#bshellz"],
@@ -10,6 +12,14 @@ var config={
     realName:"Brian",
     userName:"danleyb2"
 };
+
+var server=http.createServer(function(req,res){
+    console.log(res);
+    if (req.method=='GET' && req.url=='/') {
+	res.writeHead(200,"{'Content-type':'text/plain'}");
+	fs.createReadStream("./assets/log.txt").pipe(res);
+	}
+   });
 
 var time=new sch.RecurrenceRule();
 time.dayOfWeek=[1,3,5];
@@ -25,21 +35,38 @@ sch.scheduleJob(time,function(){
     }
     
     );
-    
-     myBot.addListener("join",function(channel,who){
+      myBot.addListener("join",function(channel,who){
+        
      if (who==config.name) {
-         myBot.say(channel,"!keep danleyb2");
+         logEvent(logfile,"Bot joinned "+channel);
+         var msg="!keep danleyb2";
+         myBot.say(channel,msg);
+         logEvent(logfile,"Bot said:- "+msg);
+       // myBot.part("#bshellz");
          
      }
-     
+    });
+  
 });
- 
-    
-});
+function insertDate(){
+    var d = new Date(),
+        dformat = [ (d.getMonth()+1),
+                    d.getDate(),
+                    d.getFullYear()].join('/')+
+                    '  ' +
+                  [ d.getHours(),
+                    d.getMinutes(),
+                    d.getSeconds()].join(':');
+          return dformat;
+}
 
-http.createServer(function(req,res){
-    
+function logEvent(fileto,str){
+    fs.open(fileto,'a');
+    fs.appendFile(fileto,insertDate()+" : "+str+"\n");
+   
+}
 
-}).listen(3000,function () {
-    
+
+server.listen(3000,function () {
+    logEvent(logfile,"Server running");
 });
