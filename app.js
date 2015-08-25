c3f84129ffb5214a2e2b925f428abe60bac577f5
danleyb2 <ndieksman@gmail.com>
2015-08-25 17:48:19 +0300

    integrating mongo Db

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var irc=require("irc");
 var fs=require("fs");
 var express=require('express');
-
+var mongo=require('mongodb').MongoClient;
+var url='mongodb://brian:brian123@ds033143.mongolab.com:33143/nodetest2';
+var database=null;
 var app=express();
 
 var logfile="./assets/log.txt";
@@ -15,11 +17,46 @@ var config={
     userName:"danleyb2"
 };
 
+mongo.connect(url,function(err,db){
+    if(err){
+        console.log('Could not connect to db');
+
+    }else{
+        console.log('Connected to db');
+        database=db;
+    }
+
+});
+
 app.get('/',function(req,res,next){
     console.log(req.url);
 
-	res.writeHead(200,"{'Content-type':'text/plain'}");
-	fs.createReadStream(logfile).pipe(res);
+    database.collection('keepShell',function(err,collection){
+        if(err){
+
+        }else{
+            //res=collection.find();
+            var results=collection.find().toArray(function(e,result){return result});
+
+            console.log(results);
+            //res.writeHead(200, {"Content-Type": "application/json"});
+            //res.end(JSON.stringify(results));
+
+            res.writeHead(200,"{'Content-type':'text/plain'}");
+            fs.createReadStream(logfile).pipe(res);
+            next();
+
+
+
+
+        }
+
+    });
+
+
+
+	//res.writeHead(200,"{'Content-type':'text/plain'}");
+	//fs.createReadStream(logfile).pipe(res);
     next();
 
    });
@@ -43,26 +80,65 @@ var myBot=new irc.Client(config.server,config.name,
 function bot(){
     myBot.connect(5,function(data){
 
+        var coll=database.collection('keepShell');
+
         console.log("Connected");
         myBot.join(config.channel,function(data){
+            console.log(data);
+            //myBot.function (nick, to, text, message) { }
 
-            logEvent(logfile,"Bot ("+data+") joined "+config.channel);
+            myBot.addListener('message', function(from, to, message) {
+
+                if(from!=config.name) {
+                    var incoming={
+                        "event":"message",
+                        "to":to,
+                        "content":message
+                    };
+                    coll.insert(incoming);
+                }
+                if(message.indexOf('ndieksBot')!=-1){
+                    myBot.disconnect(function(data){
+
+                        coll.insert({
+                            "event":"disconnect",
+                            "message":"Bot disconnected from " + config.channel
+                        });
+
+
+
+                    });
+                }
+
+
+
+            });
+
+            coll.insert({
+                "event":"join",
+                "message":"Bot ["+data+"] joinned "+config.channel
+
+            });
+
+
 
             myBot.say(config.channel,msg);
-            myBot.disconnect(function(data){
+           /* myBot.disconnect(function(data){
 
-                logEvent(logfile, "Bot Disconnected from " + config.channel);
-                logEvent(logfile,'________________________________________________________________________________________________');
+                coll.insert({
+                    "event":"disconnect",
+                    "message":"Bot disconnected from " + config.channel
+                });
 
-            })
+
+
+            });*/
 
 
         });
 
     });
 }
-
-
 
 
 function insertDate(){
@@ -76,9 +152,5 @@ function insertDate(){
                     d.getSeconds()].join(':');
           return dformat;
 }
-function logEvent(fileto,str){
-    fs.open(fileto,'a');
-    fs.appendFile(fileto,insertDate()+" : "+str+"\n");
 
-}
 module.exports=app;
